ffcf4b3b805da3573d2cabdcebb1f434873d7d9f
Junho Yeo <i@junho.io>
2023-07-07 03:04:44 +0900

    [web-ui] Setup TailwindCSS

--- threads-web-ui/src/pages/_app.tsx
+++ threads-web-ui/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import React from 'react';
 import { GlobalStyle } from '@/components/GlobalStyle';
+import '@/styles/global.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
---
+++ threads-web-ui/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-archivo)'],
+      },
+    },
+  },
+};
