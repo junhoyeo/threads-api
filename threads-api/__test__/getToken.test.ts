d1927867b508019a0555e5c914e7b72dce3c111c
iamiks <rmstjr1030@naver.com>
2023-07-10 00:16:48 +0900

    Feat: Add get token test code

---
+++ threads-api/__test__/getToken.test.ts
@@ -0,0 +1,20 @@
+import { ThreadsAPI } from '../src/threads-api';
+
+describe('getToken', () => {
+  describe('login from instagram', () => {
+    let threadsAPI: ThreadsAPI;
+    let token: string | undefined;
+
+    beforeAll(async () => {
+      // given
+      threadsAPI = new ThreadsAPI({ verbose: true, username: "username", password: "password" });
+      // when
+      token = await threadsAPI.getToken();
+    });
+
+    it('should return token', async () => {
+      // then
+      expect(typeof token).toBe('string')
+    });
+  });
+});
