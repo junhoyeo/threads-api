b65174b69e9c31048b8058776bcc00ceb046643b
Junho Yeo <i@junho.io>
2023-07-09 23:25:15 +0900

    [web-ui] wip

--- threads-web-ui/src/pages/index.tsx
+++ threads-web-ui/src/pages/index.tsx
@@ -2,7 +2,7 @@ import { NextPage } from 'next';
 
 const HomePage: NextPage = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <header className="flex flex-col gap-5 pt-[64px] pb-12 px-4 items-center rounded-3xl">
         <h1 className="text-6xl font-black tracking-tight text-center">
           Making Threads <br />
@@ -10,6 +10,8 @@ const HomePage: NextPage = () => {
         </h1>
         <p className="text-slate-400">Unofficial, Reverse-Engineered Clients for Threads.</p>
       </header>
+
+      <div className="bg-[rgba(243,245,247,0.15)] w-[200px] h-[200px] rounded-[30px]"></div>
     </div>
   );
 };
