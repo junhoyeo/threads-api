a4ec1074e5a0faf5b087cfc829e0b6be9f26e4d3
Junho Yeo <i@junho.io>
2023-07-13 12:49:22 +0900

    [web-ui] Revert tailwind.config.js

--- threads-web-ui/tailwind.config.js
+++ threads-web-ui/tailwind.config.js
@@ -1,12 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class'],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
+  content: ['./components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', '../node_modules/react-threads/**/*'],
   theme: {
     container: {
       center: true,
