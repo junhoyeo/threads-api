9a76d16afd4f442ceccd5348b782efec208117b4
Junho Yeo <i@junho.io>
2023-07-14 10:13:16 +0900

    [api] Add tests

--- threads-api/__test__/getUserProfileReplies.test.ts
+++ threads-api/__test__/getUserProfileReplies.test.ts
@@ -1,6 +1,8 @@
 import { ThreadsAPI } from '../src/threads-api';
+import { TIMEOUT, credentials } from './utils/constants';
+import { describeIf } from './utils/describeIf';
 
-test('getUserProfileReplies', async () => {
+test('getUserProfileReplies (without auth)', async () => {
   // given
   const threadsAPI = new ThreadsAPI();
   const userID = '5438123050';
@@ -12,4 +14,44 @@ test('getUserProfileReplies', async () => {
   expect(Array.isArray(posts)).toBe(true);
   expect(posts[0]).toHaveProperty('thread_items');
   expect(posts[0].thread_items.length).toBeGreaterThanOrEqual(2);
+});
+
+describeIf(!!credentials)('getUserProfileRepliesLoggedIn (with auth)', () => {
+  const threadsAPI = new ThreadsAPI({
+    verbose: true,
+    token: process.env.TOKEN,
+  });
+
+  it(
+    'Fetch user repiles with pagination.',
+    async () => {
+      // given
+      const userID = '5438123050';
+
+      // when
+      let { threads, next_max_id } = await threadsAPI.getUserProfileRepliesLoggedIn(userID);
+      const firstPageNextMaxID = next_max_id;
+      console.log('[FIRST PAGE]', next_max_id);
+
+      // then
+      expect(Array.isArray(threads)).toBe(true);
+      expect(threads[0]).toHaveProperty('thread_items');
+      expect(threads[0].thread_items[0]).toHaveProperty('post');
+      expect(next_max_id).toBeTruthy();
+
+      // next page
+      const res = await threadsAPI.getUserProfileRepliesLoggedIn(userID);
+      threads = res.threads;
+      next_max_id = res.next_max_id;
+      console.log('[SECOND PAGE]', next_max_id);
+
+      expect(Array.isArray(threads)).toBe(true);
+      expect(threads[0]).toHaveProperty('thread_items');
+      expect(threads[0].thread_items[0]).toHaveProperty('post');
+
+      expect(next_max_id).toBeTruthy();
+      expect(next_max_id).not.toEqual(firstPageNextMaxID);
+    },
+    TIMEOUT,
+  );
 });
--- threads-api/__test__/getUserProfileThreads.test.ts
+++ threads-api/__test__/getUserProfileThreads.test.ts
@@ -1,6 +1,8 @@
 import { ThreadsAPI } from '../src/threads-api';
+import { TIMEOUT, credentials } from './utils/constants';
+import { describeIf } from './utils/describeIf';
 
-test('getUserProfileThreads', async () => {
+test('getUserProfileThreads (without auth)', async () => {
   // given
   const threadsAPI = new ThreadsAPI();
   const userID = '5438123050';
@@ -12,4 +14,44 @@ test('getUserProfileThreads', async () => {
   expect(Array.isArray(posts)).toBe(true);
   expect(posts[0]).toHaveProperty('thread_items');
   expect(posts[0].thread_items[0]).toHaveProperty('post');
+});
+
+describeIf(!!credentials)('getUserProfileThreadsLoggedIn (with auth)', () => {
+  const threadsAPI = new ThreadsAPI({
+    verbose: true,
+    token: process.env.TOKEN,
+  });
+
+  it(
+    'Fetch user threads with pagination.',
+    async () => {
+      // given
+      const userID = '5438123050';
+
+      // when
+      let { threads, next_max_id } = await threadsAPI.getUserProfileThreadsLoggedIn(userID);
+      const firstPageNextMaxID = next_max_id;
+      console.log('[FIRST PAGE]', next_max_id);
+
+      // then
+      expect(Array.isArray(threads)).toBe(true);
+      expect(threads[0]).toHaveProperty('thread_items');
+      expect(threads[0].thread_items[0]).toHaveProperty('post');
+      expect(next_max_id).toBeTruthy();
+
+      // next page
+      const res = await threadsAPI.getUserProfileThreadsLoggedIn(userID);
+      threads = res.threads;
+      next_max_id = res.next_max_id;
+      console.log('[SECOND PAGE]', next_max_id);
+
+      expect(Array.isArray(threads)).toBe(true);
+      expect(threads[0]).toHaveProperty('thread_items');
+      expect(threads[0].thread_items[0]).toHaveProperty('post');
+
+      expect(next_max_id).toBeTruthy();
+      expect(next_max_id).not.toEqual(firstPageNextMaxID);
+    },
+    TIMEOUT,
+  );
 });
