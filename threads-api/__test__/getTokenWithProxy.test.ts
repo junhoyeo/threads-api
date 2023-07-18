bb593c2a4736f42d20f5b756171e5f971aa895ae
Enggar A <cyber4rt000@gmail.com>
2023-07-19 01:03:42 +0700

    Update getTokenWithProxy.test.ts
    can you double check?

--- threads-api/__test__/getTokenWithProxy.test.ts
+++ threads-api/__test__/getTokenWithProxy.test.ts
@@ -1,19 +1,24 @@
 import { ThreadsAPI } from '../src/threads-api';
 import { TIMEOUT, rawCredentials as credentials } from './utils/constants';
 import { describeIf } from './utils/describeIf';
-import * as HttpsProxyAgent from "https-proxy-agent";
+import * as HttpsProxyAgent from 'https-proxy-agent';
+import dotenv from 'dotenv';
 
-// Proxy server details
-var proxy = 'http://PROXYUSERNAME:PROXYPWD@PROXYHOST:PROXYPORT';
+dotenv.config();
+
+// Retrieve the proxy URL from the environment variables
+const proxy = process.env.PROXY;
 
 describeIf(!!credentials)('getToken', () => {
+  const HttpsProxyAgentSpy = jest.spyOn(HttpsProxyAgent, 'HttpsProxyAgent').mockImplementation(() => {});
+
   const threadsAPI = new ThreadsAPI({
     verbose: true,
-    httpsAgent: new HttpsProxyAgent.HttpsProxyAgent(proxy),
+    httpsAgent: new HttpsProxyAgent(proxy),
     ...credentials,
   });
 
-  describe('login from instagram', () => {
+  describe('login from Instagram', () => {
     let token: string | undefined;
 
     beforeAll(async () => {
@@ -29,5 +34,9 @@ describeIf(!!credentials)('getToken', () => {
       },
       TIMEOUT,
     );
+  });
+
+  it('should use the correct agent/proxy URL', () => {
+    expect(HttpsProxyAgentSpy).toHaveBeenCalledWith(proxy);
   });
 });
